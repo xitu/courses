9082ac6be2e1735695bea33afc038e705dbaf9a3
akira-cn <19511344@qq.com>
2022-06-02 11:50:48 +0800

    添加TDK，改进SEO

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useCurrentDoc } from 'docz'
 
 const footerStyle = {
     height: '3.5rem',
@@ -15,18 +16,26 @@ const linkStyle = {
 }
 
 // The doc prop contains some metadata about the page being rendered that you can use.
-const Wrapper = ({ children, doc }) => <React.Fragment>
-    <Helmet>
-        <meta charSet="utf-8" />
-        <title>掘金 - </title>
-        <link rel="icon"
-            type="image/png"
-            href="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/apple-touch-icon.png"
-        />
-    </Helmet>
-    {children}
-    <footer style={footerStyle}>
-        &copy;2022 <a href="https://juejin.cn" rel="noreferrer" target="_blank" style={linkStyle}>稀土掘金</a>
-    </footer>
-</React.Fragment>
+const Wrapper = ({ children }) => {
+    const currentDoc = useCurrentDoc()
+    const title = currentDoc.menu ? `${currentDoc.menu} ${currentDoc.name}` : currentDoc.name
+    const description = currentDoc.desc || title;
+    const keywords = currentDoc.keywords || title;
+    return (<React.Fragment>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>掘金 - {title}</title>
+            <meta name="description" content={description}/>
+            <meta name="keywords" content={keywords}/>
+            <link rel="icon"
+                type="image/png"
+                href="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/apple-touch-icon.png"
+            />
+        </Helmet>
+        {children}
+        <footer style={footerStyle}>
+            &copy;2022 <a href="https://juejin.cn" rel="noreferrer" target="_blank" style={linkStyle}>稀土掘金</a>
+        </footer>
+    </React.Fragment>)
+}
 export default Wrapper
