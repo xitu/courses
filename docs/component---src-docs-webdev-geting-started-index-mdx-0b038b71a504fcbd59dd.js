28b0226762ddf6778e5d88ca6cd1f5c7313d730e
akira-cn <19511344@qq.com>
2022-06-04 16:00:55 +0800

    update docs

---
+++ docs/component---src-docs-webdev-geting-started-index-mdx-0b038b71a504fcbd59dd.js
@@ -0,0 +1,166 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["component---src-docs-webdev-geting-started-index-mdx"],{
+
+/***/ "cjYC":
+/*!***************************************************!*\
+  !*** ../src/docs/webdev/geting_started/index.mdx ***!
+  \***************************************************/
+/*! exports provided: _frontmatter, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "_frontmatter", function() { return _frontmatter; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return MDXContent; });
+/* harmony import */ var _babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/extends */ "Fcif");
+/* harmony import */ var _babel_runtime_helpers_esm_objectWithoutPropertiesLoose__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/esm/objectWithoutPropertiesLoose */ "+I+c");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "mXGw");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @mdx-js/react */ "/FXl");
+/* harmony import */ var _Users_wuliang_WorkSpace_localized_courses_node_modules_gatsby_theme_docz_src_base_Layout_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../node_modules/gatsby-theme-docz/src/base/Layout.js */ "TjRS");
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @emotion/core */ "aD51");
+
+
+var _excluded = ["components"];
+
+/* @jsx mdx */
+
+
+/* @jsxRuntime classic */
+
+/* @jsx mdx */
+
+
+
+var _frontmatter = {};
+
+if (typeof _frontmatter !== 'undefined' && _frontmatter && _frontmatter === Object(_frontmatter) && Object.isExtensible(_frontmatter) && !Object.prototype.hasOwnProperty.call(_frontmatter, '__filemeta')) {
+  Object.defineProperty(_frontmatter, '__filemeta', {
+    configurable: true,
+    value: {
+      name: "_frontmatter",
+      filename: "src/docs/webdev/geting_started/index.mdx"
+    }
+  });
+}
+
+var layoutProps = {
+  _frontmatter: _frontmatter
+};
+var MDXLayout = _Users_wuliang_WorkSpace_localized_courses_node_modules_gatsby_theme_docz_src_base_Layout_js__WEBPACK_IMPORTED_MODULE_4__["default"];
+function MDXContent(_ref) {
+  var components = _ref.components,
+      props = Object(_babel_runtime_helpers_esm_objectWithoutPropertiesLoose__WEBPACK_IMPORTED_MODULE_1__["default"])(_ref, _excluded);
+
+  return Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])(MDXLayout, Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({}, layoutProps, props, {
+    components: components,
+    mdxType: "MDXLayout"
+  }), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("h1", {
+    "id": "写给普通人看的网页开发课"
+  }, "\u5199\u7ED9\u666E\u901A\u4EBA\u770B\u7684\u7F51\u9875\u5F00\u53D1\u8BFE"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", null, "\u96F6\u57FA\u7840\u5165\u95E8Web\u5F00\u53D1\uFF0C\u624B\u628A\u624B\u6559\u4F60\u642D\u5EFA\u4E00\u4E2A\u7B80\u5355\u7684\u535A\u5BA2\u7CFB\u7EDF\u3002"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", null, "\u901A\u8FC7\u8FD9\u95E8\u8BFE\uFF0C\u4F60\u5C06\u5B66\u4F1A\uFF1A"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("ul", null, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", {
+    parentName: "li"
+  }, "HTML5\u57FA\u7840\u77E5\u8BC6\uFF1A"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("ul", {
+    parentName: "li"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u8BED\u4E49\u5316\u6807\u7B7E"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u5C5E\u6027"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u9875\u9762\u7ED3\u6784"))), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", {
+    parentName: "li"
+  }, "CSS\u57FA\u7840\u77E5\u8BC6"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("ul", {
+    parentName: "li"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u9009\u62E9\u5668"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u5143\u7D20\u76D2\u5B50"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u5E03\u5C40\u548C\u6837\u5F0F"))), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", {
+    parentName: "li"
+  }, "JS\u57FA\u7840\u77E5\u8BC6"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("ul", {
+    parentName: "li"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u6D4F\u89C8\u5668\u4E2D\u7684JavaScript"))), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("p", {
+    parentName: "li"
+  }, "\u5B9E\u73B0\u4E00\u4E2A\u7B80\u5355\u7684\u4E2A\u4EBA\u535A\u5BA2\u5E76\u901A\u8FC7Github Pages\u53D1\u5E03"))), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("h2", {
+    "id": "课程目录"
+  }, "\u8BFE\u7A0B\u76EE\u5F55"), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("ul", null, Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C1\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "what_is_fe"
+  }, "\u4EC0\u4E48\u662F\u524D\u7AEF\u5DE5\u7A0B\u5E08")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C2\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "design_a_blog"
+  }, "\u8BBE\u8BA1\u7B80\u5355\u7684\u4E2A\u4EBA\u535A\u5BA2\u5427")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C3\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "learn_html5"
+  }, "\u521D\u8BC6HTML5")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C4\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "html_and_blog"
+  }, "\u7528HTML\u7EC4\u7EC7\u535A\u5BA2\u5185\u5BB9")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C5\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "learn_css"
+  }, "\u521D\u8BC6CSS")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C6\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "element_box"
+  }, "\u5143\u7D20\u76D2\u5B50")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C7\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "display_attribute"
+  }, "display\u5C5E\u6027")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C8\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "layout_and_styles"
+  }, "\u5E03\u5C40\u548C\u6837\u5F0F")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C10\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "javascript_in_page"
+  }, "\u7528JS\u7ED9\u7F51\u9875\u6765\u70B9\u53D8\u5316")), Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("li", {
+    parentName: "ul"
+  }, "\u7B2C11\u8282\uFF1A", Object(_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__["mdx"])("a", {
+    parentName: "li",
+    "href": "publish_your_blog_work"
+  }, "\u53D1\u5E03\u4F60\u7684\u535A\u5BA2\u4F5C\u54C1"))));
+}
+
+if (typeof MDXContent !== 'undefined' && MDXContent && MDXContent === Object(MDXContent) && Object.isExtensible(MDXContent) && !Object.prototype.hasOwnProperty.call(MDXContent, '__filemeta')) {
+  Object.defineProperty(MDXContent, '__filemeta', {
+    configurable: true,
+    value: {
+      name: "MDXContent",
+      filename: "src/docs/webdev/geting_started/index.mdx"
+    }
+  });
+}
+
+;
+MDXContent.isMDXComponent = true;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=component---src-docs-webdev-geting-started-index-mdx-0b038b71a504fcbd59dd.js.map
